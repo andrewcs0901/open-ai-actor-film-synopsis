51d45c0c6145530dea525ab75f343debfa7d5d1b
andrewcs0901 <andrewcs0901@gmail.com>
2023-06-18 20:30:48 -0300

    feat: created initial version of poem generator

---
+++ src/test/kotlin/com/andrew/poemgenerator/PoemGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.andrew.poemgenerator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PoemGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
